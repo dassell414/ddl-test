cacd02c0d14a0d0e78f13e159fd2bda64f523afb
nwat-b <nwat-b@users.noreply.github.com>
2014-10-27 15:04:35 +0000

    Update AppMeasurement_ChASE_141023_004.js

--- AppMeasurement_ChASE_141023_004.js
+++ AppMeasurement_ChASE_141023_004.js
@@ -202,7 +202,7 @@ var wa_view = function() {
 	logJson();
 	
 	//  ... and reset everything
-	s.contextData = '';
+	s.contextData = {};
 	dprop = '';
 }
 
@@ -258,7 +258,7 @@ var wa_action = function(evtNm) {
 	logJson();
 	
 	//  ... and reset everything
-	s.contextData = '';
+	s.contextData = {};
 	dprop = '';
 }
 
